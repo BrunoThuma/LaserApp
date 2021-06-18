3e10cf0455e9b234b65adcfa69352ca33fad9a06
Bruno Thuma <bruno.thuma@hotmail.com>
2021-06-18 11:55:10 -0300

    Initial Commit

---
+++ LaserApp/LaserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaserAppApp.swift
+//  LaserApp
+//
+//  Created by Bruno Thuma on 18/06/21.
+//
+
+import SwiftUI
+
+@main
+struct LaserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
