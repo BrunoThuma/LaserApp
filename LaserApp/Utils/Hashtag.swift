5bdee8164d0cd2f35623d14adde0c84c3812b28e
gkum4 <gustavokumasawa@gmail.com>
2021-06-23 11:26:35 -0300

    Feat Hashtag and PersonalWorkspaceCard utils

---
+++ LaserApp/Utils/Hashtag.swift
@@ -0,0 +1,34 @@
+//
+//  Hashtag.swift
+//  Laser
+//
+//  Created by Gustavo Kumasawa on 23/06/21.
+//
+
+import SwiftUI
+
+struct HashtagData: Identifiable {
+    let id = UUID()
+    let title: String
+    let color: String
+}
+
+struct Hashtag: View {
+    var data: HashtagData
+    
+    var body: some View {
+        Group {
+            Text(data.title)
+                .font(.system(size: 8, weight: .regular))
+        }
+        .padding(5)
+        .background(Color(data.color))
+        .cornerRadius(14)
+    }
+}
+
+//struct Hashtag_Previews: PreviewProvider {
+//    static var previews: some View {
+//        Hashtag()
+//    }
+//}
